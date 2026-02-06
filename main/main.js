2b87b53f2375fb6f05b4afae6e6201ce047343a2
Nathan <n.rinalduzzi@gmail.com>
2026-02-06 18:21:23 +0100

    menu removed wind

--- main/main.js
+++ main/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
 
 function createWindow () {
@@ -12,6 +12,7 @@ function createWindow () {
       contextIsolation: true
     }
   });
+  win.removeMenu(); 
 
   win.loadFile(path.join(__dirname, '../renderer/index.html'));
 }
