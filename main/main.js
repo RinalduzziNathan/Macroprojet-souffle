9209a7d836ddb2b741594eaaab9c907365fd16fe
Nathan <n.rinalduzzi@gmail.com>
2026-02-06 18:42:23 +0100

    icons upload

--- main/main.js
+++ main/main.js
@@ -5,7 +5,7 @@ function createWindow () {
   const win = new BrowserWindow({
     width: 1280,
     height: 800,
-    icon: '/../renderer/build/icon.ico',
+    icon: '/../renderer/icon/icon.icns',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: false,
